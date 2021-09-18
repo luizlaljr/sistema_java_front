f96ce88c64a93f10285795435ffe1fe577eaaa2d
luizlaljr <luizlaljr@gmail.com>
2021-09-18 07:33:17 -0300

    Criada pagina principal

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { myNewTheme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={myNewTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
-
 export default MyApp
